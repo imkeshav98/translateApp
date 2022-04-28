f02d78ff484325eb85b8875bcdf9057a647ba957
Keshav Chaudhary <97694772+imkeshav98@users.noreply.github.com>
2022-04-29 02:13:17 +0530

    Update function.js

--- function.js
+++ function.js
@@ -1,1 +1,29 @@
 
+async function getData() {
+  let input = document.getElementById("input-text").value;
+  let inputLan = document.getElementById("lang-val").value;
+  let inputLanSour = document.getElementById("lang-val-source").value;
+
+  try {
+    let res = await fetch("https://libretranslate.de/translate", {
+      method: "POST",
+      body: JSON.stringify({
+        q: input,
+        source: inputLanSour,
+        target: inputLan,
+        format: "text",
+      }),
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    let data = await res.json();
+    console.log(data.translatedText);
+
+    document.getElementById("translated-text").innerHTML = data.translatedText;
+  } catch (err) {
+    console.log(err);
+  }
+}
